13c6ca53b41cc87aae5d65ad9c3f9dda80466c46
hajdukka <nerminhajdo@gmail.com>
2021-03-11 21:04:53 +0100

    dodao ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,19 @@
+function main() {
+    
+    // Only change code below this line
+  
+    var myDecimal = 4.5;
+
+    var myDecimalE = 254e-5;
+  
+    // Only change code above this line
+  
+    return {
+        myDecimal,
+        myDecimalE
+    };
+  }
+  
+  console.log(main());
+  module.exports = main;
+  
