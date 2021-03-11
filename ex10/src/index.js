97e3992ed0c5bb3d779f641b09fb50d3fe830844
hajdukka <nerminhajdo@gmail.com>
2021-03-11 22:25:08 +0100

    dodao ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,21 @@
+function main() {
+  // Only change code below this line
+
+  var myFirstName;
+  var myLastName;
+  var myHomeTown;
+
+  myFirstName = "Nermin";
+  myLastName = "Hajdo";
+  myHomeTown = "Sarajevo";
+
+  // Only change code above this line
+
+  return {
+    myFirstName,
+    myLastName,
+    myHomeTown,
+  };
+}
+console.log(main());
+module.exports = main;
