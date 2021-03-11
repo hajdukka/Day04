c7f66748e199b9739ce138b94e4728a75cb8f25f
hajdukka <nerminhajdo@gmail.com>
2021-03-11 22:18:52 +0100

    dodao ex09

---
+++ ex09/src/index.js
@@ -0,0 +1,25 @@
+function main() {
+  // Only change code below this line
+
+  var firstNum;
+  var secondNum;
+  var thirdNum;
+
+  var firstNum = 6;
+  var secondNum = 25;
+  var thirdNum = 5.2;
+
+  firstNum *= 9;
+  thirdNum *= 10;
+  secondNum /= 5;
+
+  // Only change code above this line
+
+  return {
+    firstNum,
+    secondNum,
+    thirdNum,
+  };
+}
+console.log(main());
+module.exports = main;
